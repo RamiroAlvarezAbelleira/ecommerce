c20674cad38645ce2b55bfc1596e70c1860600f3
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2022-07-09 17:34:00 -0300

    creadas las tarjetas de productos en el home con el slider y el nav de version mobile

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const publicPath = path.resolve(__dirname, './public')
 
 const app = express()
 
+const port = 3000;
+
 app.use( express.static(publicPath))
 
-app.listen(3000, () => console.log('servidor funcionando'));
+app.listen(port, () => console.log(`servidor funcionando en el puerto ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
