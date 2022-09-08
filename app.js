36355df9bfcd71da41f50a9d42f8d8695c9591cc
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2022-09-07 21:06:29 -0300

    update

--- app.js
+++ app.js
@@ -1,56 +1,36 @@
 const express = require('express');
-
 const path = require('path');
-
 const methodOverride = require('method-override');
-
 const publicPath = path.join(__dirname, '../public');
-
 const app = express();
-
-const port = 3000;
-
-const mainRoutes = require('./routes/mainRoutes');
-
-const productRoutes = require('./routes/productRoutes');
-
-const userRoutes = require('./routes/userRoutes');
-
+const port = 8080;
+const mainRoutes = require('./src/routes/mainRoutes');
+const productRoutes = require('./src/routes/productRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 const session = require('express-session');
-
-const loggedUserMW = require('./middleware/loggedUserMW');
+const loggedUserMW = require('./src/middleware/loggedUserMW');
 
 /*SETTING*/
 
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(publicPath));
-
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
-
 app.use(methodOverride("_method"));
-
 app.use(session({
     secret: 'shhh',
     resave: false,
     saveUninitialized: false
 }));
-
 app.use(loggedUserMW);
 
 /*RUTAS*/
 
 app.use('/', mainRoutes);
-
 app.use('/productos', productRoutes);
-
 app.use('/usuarios', userRoutes)
-
-app.listen(process.env.PORT ||port, () => console.log(`servidor funcionando en el puerto ${port}`));
+app.listen(process.env.PORT || port, () => console.log(`servidor funcionando en el puerto ${port}`));
 
 
 
